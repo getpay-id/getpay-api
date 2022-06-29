a3ce11791a2d2e1b4e72a55108fbb97083717efa
aprilahijriyan <hijriyan23@gmail.com>
2022-06-30 06:00:04 +0700

    🐞 fix(schema): perbaikan petunjuk ketik dan tipe bidang

--- app/core/fields.py
+++ app/core/fields.py
@@ -2,7 +2,7 @@ import os
 from typing import Callable, Generator
 
 from bson import ObjectId as _ObjectId
-from pydantic import AnyHttpUrl, AnyUrl, BaseConfig
+from pydantic import AnyUrl, BaseConfig
 from pydantic.fields import ModelField
 
 
@@ -12,20 +12,19 @@ class ObjectID(str):
         yield cls.validate
 
     @classmethod
-    def validate(cls, oid: str) -> _ObjectId:
+    def validate(cls, oid: str) -> str:
         if not isinstance(oid, str):
-            raise TypeError("ObjectID must be a string")
+            raise TypeError("ID must be a string")
         if not _ObjectId.is_valid(oid):
-            raise ValueError("ID is not valid")
+            raise ValueError("Invalid ID")
         return oid
 
 
-class Image(AnyHttpUrl):
+class Image(str):
     @classmethod
     def validate(
         cls, value: str, field: "ModelField", config: "BaseConfig"
     ) -> "AnyUrl":
-        super().validate(value, field, config)
         _, ext = os.path.splitext(os.path.basename(value))
         if ext not in [".jpg", ".jpeg", ".png", ".svg"]:
             raise ValueError("Image required")
