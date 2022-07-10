41b05a2a39230fc78b60ce04cc1441f1737ba8d6
aprilahijriyan <hijriyan23@gmail.com>
2022-07-11 06:50:57 +0700

    🧪 test: menambahkan test autentikasi

---
+++ tests/test_auth.py
@@ -0,0 +1,18 @@
+import os
+
+import pytest
+
+from .lib.client import client
+
+USER_EMAIL = os.environ.get("USER_EMAIL")
+USER_PASSWORD = os.environ.get("USER_PASSWORD")
+
+
+def test_signin(request: pytest.FixtureRequest):
+    response = client.post(
+        "/auth/signin", data={"username": USER_EMAIL, "password": USER_PASSWORD}
+    )
+    assert response.status_code == 200
+    data = response.json()
+    assert all(key in data for key in ["detail", "access_token", "token_type"])
+    request.config.cache.set("jwt", data["access_token"])
