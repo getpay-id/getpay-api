70c75bfff0381bb788df55eb94e07b746025cbe2
aprilahijriyan <hijriyan23@gmail.com>
2022-07-12 05:50:07 +0700

    🎈 perf(tests): hapus print statement

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -12,9 +12,6 @@ def test_signin(request: pytest.FixtureRequest):
     response = client.post(
         "/auth/signin", data={"username": USER_EMAIL, "password": USER_PASSWORD}
     )
-    print(response.json())
-    print("Username:", USER_EMAIL)
-    print("Password:", USER_PASSWORD)
     assert response.status_code == 200
     data = response.json()
     assert all(key in data for key in ["detail", "access_token", "token_type"])
