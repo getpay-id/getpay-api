541c26bea5cd518f9f97446bb0b840fe91d88c7f
aprilahijriyan <hijriyan23@gmail.com>
2022-07-11 16:31:19 +0700

    ✨ feat(tests): menambahkan fixture apikey_headers

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,3 +10,12 @@ def auth_headers(request: pytest.FixtureRequest):
         warnings.warn("No JWT found.")
 
     return {"Authorization": f"Bearer {token}"}
+
+
+@pytest.fixture
+def apikey_headers(request: pytest.FixtureRequest):
+    apikey_object = request.config.cache.get("apikey_object", None)
+    if not apikey_object:
+        warnings.warn("No API Key found.")
+
+    return {"Authorization": f"Bearer {apikey_object['api_key']}"}
