cadc5e3611d16d78b4654488dd4e69a14af2fba2
aprilahijriyan <hijriyan23@gmail.com>
2022-08-02 09:20:23 +0700

    🎈 perf(extensions): konek ke redis ketika app berjalan

--- app/extensions/__init__.py
+++ app/extensions/__init__.py
@@ -10,3 +10,10 @@ def init_extensions(app: FastAPI):
     cache.setup(app)
     saq.setup(app)
     ratelimit.setup(app)
+
+    async def on_startup():
+        # todo: hapus jika popol sudah menyediakan fitur ini
+        cache_backend = app.state.cache
+        await cache_backend.connect()
+
+    app.add_event_handler("startup", on_startup)
