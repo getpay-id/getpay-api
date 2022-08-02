8ac5b9663583d0dfaa4975ca98188a7605011240
aprilahijriyan <hijriyan23@gmail.com>
2022-08-03 03:36:18 +0700

    🦄 refactor(websocket): perbaikan websocket untuk mendapatkan status transaksi secara realtime

--- app/routers/ws/views.py
+++ app/routers/ws/views.py
@@ -1,4 +1,8 @@
+import asyncio
+
+import async_timeout
 from aioredis.client import PubSub
+from bson import ObjectId
 from popol.cache.globals import cache
 from starlette import status
 from starlette.websockets import WebSocket
@@ -7,25 +11,44 @@ from app.core.constants import REDIS_PREFIX_TRANSACTION_CHANNEL
 from app.core.websocket import BaseWebSocket
 
 
+async def _get_trx_id(websocket: WebSocket) -> str:
+    trx_id: str = websocket.query_params.get("trx_id", None)
+    if not trx_id:
+        await websocket.close(status.WS_1013_TRY_AGAIN_LATER)
+
+    if not ObjectId.is_valid(trx_id):
+        await websocket.close(status.WS_1003_UNSUPPORTED_DATA)
+
+    return trx_id
+
+
 class GetTransactionStatus(BaseWebSocket):
-    async def on_receive(self, websocket: WebSocket, data: dict) -> None:
-        if not isinstance(data, dict):
-            await self.throw(websocket, status.WS_1007_INVALID_FRAME_PAYLOAD_DATA)
-
-        trans_id = data.get("id")
-        if not trans_id:
-            await self.throw(websocket, status.WS_1003_UNSUPPORTED_DATA)
-
+    async def on_connect(self, websocket: WebSocket):
+        await websocket.accept()
+        trx_id = await _get_trx_id(websocket)
         pubsub: PubSub = cache.pubsub()
-        async with pubsub as ps:
-            channel = REDIS_PREFIX_TRANSACTION_CHANNEL + trans_id
-            print("Subscribe to:", channel)
-            await ps.subscribe(channel)
-            message = await ps.get_message(ignore_subscribe_messages=True)
-            if message is not None:
-                print(f"Message Received: {message}")
-                data = message["data"]
-                if data != "STOP":
-                    await websocket.send_json({"status": data})
-            await ps.unsubscribe(channel)
-            print("Unsubscribe to:", channel)
+        channel = REDIS_PREFIX_TRANSACTION_CHANNEL + trx_id
+        await pubsub.subscribe(channel)
+        websocket.state.pubsub = pubsub
+        websocket.state.channel = channel
+        self.task = asyncio.create_task(self.get_realtime_status(websocket))
+
+    async def get_realtime_status(self, websocket: WebSocket):
+        pubsub: PubSub = websocket.state.pubsub
+        while not self.task.done():
+            try:
+                async with async_timeout.timeout(10):
+                    message = await pubsub.get_message(ignore_subscribe_messages=True)
+                    if message is not None:
+                        trx_status = message["data"]
+                        await websocket.send_text(trx_status)
+                        await websocket.close()
+                    await asyncio.sleep(0.1)
+            except asyncio.TimeoutError:
+                pass
+
+    async def on_disconnect(self, websocket: WebSocket, close_code: int) -> None:
+        pubsub: PubSub = websocket.state.pubsub
+        channel = websocket.state.channel
+        await pubsub.unsubscribe(channel)
+        self.task.cancel()
